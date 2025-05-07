93eb7c004bcad5949cbf51d5f01442e37fa8c10d
gopi <gopi.g@digitelenetworks.com>
2025-05-07 22:56:16 +0530

    Adding SQL folder

---
+++ sql/sql_server/1.sql
@@ -0,0 +1,25 @@
+-- USE MyDatabase
+
+-- SELECT * 
+-- FROM customers
+
+--SELECT
+--    country,
+--    score
+--FROM customers
+
+--SELECT *
+--FROM customers
+--WHERE score>0
+--ORDER BY score DESC, country 
+
+
+
+
+
+
+
+
+
+
+
